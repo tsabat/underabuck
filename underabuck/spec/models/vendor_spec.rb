67fa4895913391cac1bedcb1fcd195dddb2162b8
Timothy Sabat <tim@codepen.io>
2018-05-03 16:42:25 -0600

    database cleaner

--- underabuck/spec/models/vendor_spec.rb
+++ underabuck/spec/models/vendor_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is cool' do
+    Vendor.new(id: 1).save!
+    ap Vendor.all
+  end
 end
--- underabuck/spec/rails_helper.rb
+++ underabuck/spec/rails_helper.rb
@@ -55,3 +55,19 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# database cleaner
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
